626d808aa75c062d1f50514c1ef73e35f87bc16e
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-16 12:37:42 +0100

    Add InsufficientBalanceException

---
+++ src/main/java/org/example/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
