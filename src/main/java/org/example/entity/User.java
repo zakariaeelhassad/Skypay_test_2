b186e32a296d686e4583e6bfbe5123a8f6a6406d
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-16 12:37:22 +0100

    Add User entity

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,14 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int id ;
+    private int balance ;
+}
